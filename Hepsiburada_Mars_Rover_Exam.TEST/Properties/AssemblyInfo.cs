af47903cc0ece6db1041b7fcea3712b70db3b78e
Muhammed Emin Karaarslan <m_e_min_krs@hotmail.com>
2020-10-30 23:57:51 +0300

    Project created

---
+++ Hepsiburada_Mars_Rover_Exam.TEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hepsiburada_Mars_Rover_Exam.TEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hepsiburada_Mars_Rover_Exam.TEST")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("38f9baaf-f867-4a07-bb5c-9855a5b5d055")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
